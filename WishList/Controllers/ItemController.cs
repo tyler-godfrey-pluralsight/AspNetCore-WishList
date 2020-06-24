1df0467aa3ad09204fdb6ff74759d6c3e1f9611c
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-06-24 15:46:22 -0500

    changed variable name

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -11,9 +11,9 @@ namespace WishList.Controllers
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ItemController(ApplicationDbContext dbContext)
+        public ItemController(ApplicationDbContext context)
         {
-            _context = dbContext;
+            _context = context;
         }
 
         public IActionResult Index() 
