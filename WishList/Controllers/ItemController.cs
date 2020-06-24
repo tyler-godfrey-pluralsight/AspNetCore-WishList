bc81539d7d9e5cd07c6837f7525902a397c4e0b7
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-06-24 15:39:14 -0500

    Create The ItemController

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            _context.Items.Remove(_context.Items.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
