db161f753dd0a15c13f92193b42b7935e8214bf4
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-06-24 14:43:22 -0500

    Items moved out of ApplicationDbContext constructor.

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -9,9 +9,10 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item> Items { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            public DbSet<Item> Items { get; set; }
+            
         }
     }
 }
