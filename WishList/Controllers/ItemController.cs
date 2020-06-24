f6dd8ceaa0d7206522fa05f1dff8074f28f7168c
Tyler Godfrey <tyler.r.godfrey@emcins.com>
2020-06-24 16:01:52 -0500

    adding list<item> to itemcontroller index

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -18,7 +18,8 @@ namespace WishList.Controllers
 
         public IActionResult Index() 
         {
-            return View("Index");
+            List<Item> items = _context.Items.ToList();
+            return View("Index", items);
         }
         [HttpGet]
         public IActionResult Create()
